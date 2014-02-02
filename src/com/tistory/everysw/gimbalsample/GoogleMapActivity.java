9598fabcf5122e3faa012712e1c8196e27622ce9
juhwany <juhwany@gmail.com>
2014-02-02 17:30:06 +0900

    Added code for checking if Google Play Service is available.

--- src/com/tistory/everysw/gimbalsample/GoogleMapActivity.java
+++ src/com/tistory/everysw/gimbalsample/GoogleMapActivity.java
@@ -3,14 +3,18 @@ package com.tistory.everysw.gimbalsample;
 import java.util.ArrayList;
 
 import android.app.Activity;
+import android.app.Dialog;
 import android.content.Intent;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.FragmentActivity;
 import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesClient;
+import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.location.LocationClient;
 import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
@@ -26,7 +30,7 @@ import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.PolygonOptions;
 
-public class GoogleMapActivity extends Activity 
+public class GoogleMapActivity extends FragmentActivity 
 							implements OnMyLocationButtonClickListener,
 							GooglePlayServicesClient.ConnectionCallbacks,
 							GooglePlayServicesClient.OnConnectionFailedListener,
@@ -58,7 +62,7 @@ public class GoogleMapActivity extends Activity
 		googleMap.setOnMyLocationButtonClickListener(this);
 		googleMap.setOnMapClickListener(this);
 		googleMap.setOnMapLongClickListener(this);
-		googleMap.setOnMarkerClickListener(this);
+		googleMap.setOnMarkerClickListener(this);		
 
 		googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLocation, MAP_DEFAULT_LOCATION_ZOOM));
 		
@@ -67,18 +71,34 @@ public class GoogleMapActivity extends Activity
 	}
 	
     @Override
-    protected void onPause() {
-        super.onPause();
-        locationClient.disconnect();
-    }	
-	
+    protected void onStart() {
+        super.onStart();
+        startLocationUpdate();
+    }
 
     @Override
-	protected void onResume() {
-		super.onResume();
-		setUpLocationClientIfNeeded();
-		locationClient.connect();
-	}
+    protected void onStop() {
+        super.onStop();
+        stopLocationUpdate();        
+    }
+
+    
+    void startLocationUpdate() {
+        if(isGoogleServiceAvailable()) {
+            setUpLocationClientIfNeeded();
+            locationClient.connect();
+        }
+    }
+    
+    void stopLocationUpdate() {
+        if(isGoogleServiceAvailable()) {        
+            if(locationClient.isConnected()) {
+                locationClient.removeLocationUpdates(this);
+            }
+            
+            locationClient.disconnect();
+        }
+    }
 
     private void setUpLocationClientIfNeeded() {
         if (locationClient == null) {
@@ -88,10 +108,38 @@ public class GoogleMapActivity extends Activity
                     this);
         }
     }
+    
+    @Override
+    public void onConnected(Bundle arg0) {
+        
+        LocationRequest request = LocationRequest.create()
+                .setInterval(5000)
+                .setFastestInterval(16)
+                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        
+        locationClient.requestLocationUpdates(request, this); 
+        Location lastLocation = locationClient.getLastLocation();
+        if(lastLocation != null) {
+            currentLocation = new LatLng(lastLocation.getLatitude(), lastLocation.getLongitude());
+        }
+    }
+
+    @Override
+    public void onDisconnected() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        currentLocation = new LatLng(location.getLatitude(), location.getLongitude());
+    }    
+    
 
 	@Override
-	public boolean onMyLocationButtonClick() {
-		if(currentLocation != null) {
+	public boolean onMyLocationButtonClick() {   
+	    
+	    if(currentLocation != null) {
 			googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLocation, MAP_CURRENT_LOCATION_ZOOM));
 		}
 		
@@ -115,26 +163,7 @@ public class GoogleMapActivity extends Activity
         
     }
 
-    @Override
-    public void onConnected(Bundle arg0) {
-        LocationRequest request = LocationRequest.create()
-                .setInterval(5000)
-                .setFastestInterval(16)
-                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-        
-        locationClient.requestLocationUpdates(request, this);        
-    }
 
-    @Override
-    public void onDisconnected() {
-        // TODO Auto-generated method stub
-        
-    }
-
-    @Override
-    public void onLocationChanged(Location location) {
-        currentLocation = new LatLng(location.getLatitude(), location.getLongitude());
-    }
 
     @Override
     public void onMapClick(LatLng arg0) {
@@ -166,5 +195,59 @@ public class GoogleMapActivity extends Activity
         options.fillColor(getResources().getColor(R.color.map_polygon_fill_color));
         googleMap.addPolygon(options);
     }
+    
+    private boolean isGoogleServiceAvailable() {
+
+        // Check that Google Play services is available
+        int resultCode =
+                GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+
+        // If Google Play services is available
+        if (ConnectionResult.SUCCESS == resultCode) {
+            return true;
+        // Google Play services was not available for some reason
+        } else {
+            // Display an error dialog
+            Dialog dialog = GooglePlayServicesUtil.getErrorDialog(resultCode, this, 0);
+            if (dialog != null) {
+                ErrorDialogFragment errorFragment = new ErrorDialogFragment();
+                errorFragment.setDialog(dialog);
+                errorFragment.show(getSupportFragmentManager(), getResources().getString(R.string.app_name));
+            }
+            return false;
+        }
+    }
+    
+    public static class ErrorDialogFragment extends DialogFragment {
+
+        // Global field to contain the error dialog
+        private Dialog mDialog;
+
+        /**
+         * Default constructor. Sets the dialog field to null
+         */
+        public ErrorDialogFragment() {
+            super();
+            mDialog = null;
+        }
+
+        /**
+         * Set the dialog to display
+         *
+         * @param dialog An error dialog
+         */
+        public void setDialog(Dialog dialog) {
+            mDialog = dialog;
+        }
+
+        /*
+         * This method must return a Dialog to the DialogFragment.
+         */
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            return mDialog;
+        }
+    }
+    
 
 }
